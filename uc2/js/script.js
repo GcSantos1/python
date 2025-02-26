b4b7899f8baf9baeb50d1e6e1aada03ec60b89ef
GcSantos1 <144066966+GcSantos1@users.noreply.github.com>
2025-02-26 16:43:10 -0300

    Aula 26/02/2025
    Continuação bootstrap

--- uc2/js/script.js
+++ uc2/js/script.js
@@ -1,3 +1,12 @@
-window.onload = function(){
-    alert("Seja bem vindo a Jornada Viagens!")
+
+function exibirDados(){
+
+    let nome = document.getElementById('nome').value;
+    let tel = document.getElementById('movel').value;
+    let email = document.getElementById('email').value;
+
+    document.write('<h2>Dados Recebidos:</h2>');
+    document.write('<p><Strong>Nome:</Strong>' + nome+ '</p>');
+    document.write('<p><Strong>Telefone:</Strong>' + tel+ '</p>');
+    document.write('<p><Strong>Email:</Strong>' + email+ '</p>');
 }
